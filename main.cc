ed787264b6ba2412d6d1cbb8b822562aebaa6cad
Stefan Z <43833788+sz02@users.noreply.github.com>
2018-10-03 21:52:55 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-  cout << "Testing" << endl;
+  int a;
+  if (cin >> a) {
+    if (a % 2 == 0) cout << a/2 << endl;
+    else cout << a << endl;
+  }
+  return 0;
 }
